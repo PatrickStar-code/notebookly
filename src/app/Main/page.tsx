f160c4552820b94385b0401e822854e8b011ff16
Patrick da Silva Almeida <patrick.almeida06@gmail.com>
2024-12-20 13:12:55 -0300

    feat: search in pages note and home

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-
 import db from "@/lib/db";
 import { auth } from "@/auth";
-import { DialogNewNoteboock } from "../_components/dialogNewNotebook";
-import { Plus } from "lucide-react";
-import { ToastContainer } from "react-toastify";
-import DrawnButton from "../_components/drawnButton";
-import NotebookCard from "../_components/cardNotebook";
-import Image from "next/image";
+import HomeContent from "../_components/homeContent";
 
 export default async function Home() {
   const session = await auth();
@@ -21,79 +15,5 @@ export default async function Home() {
     },
   });
 
-  return (
-    <>
-      <div className="flex flex-col items-center text-center md:text-left md:flex-row md:justify-between">
-        <h1 className="font-serif text-4xl md:text-6xl lg:text-8xl font-edu mb-4 md:mb-0">
-          Bem vindo(a)!
-        </h1>
-        <DialogNewNoteboock
-          trigger={
-            <div>
-              {/* Botão para telas grandes */}
-              <DrawnButton
-                variant="outline"
-                className="hidden lg:flex fixed items-center gap-2 top-12 right-8 z-40"
-              >
-                Criar Caderno <Plus size={16} />
-              </DrawnButton>
-              {/* Botão para telas médias e menores */}
-              <DrawnButton
-                variant="outline"
-                className="fixed flex lg:hidden items-center py-[0.5rem] px-[0.5rem] right-8 top-8 z-40 rounded-full"
-              >
-                <Plus size={16} />
-              </DrawnButton>
-            </div>
-          }
-        />
-      </div>
-      {notebooks.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 sm:mt-8 md:mt-10">
-          {notebooks.map((notebook) => (
-            <NotebookCard {...notebook} key={notebook.id} />
-          ))}
-        </div>
-      ) : (
-        <div className="flex flex-col items-center justify-center h-[70vh] text-center">
-          <Image
-            src="/empty.svg"
-            alt="No notebooks"
-            className="w-48 h-48 mb-4"
-            width={96}
-            height={96}
-          />
-          <p className="text-2xl md:text-3xl lg:text-4xl font-medium text-black dark:text-gray-400">
-            Nenhum caderno encontrado.
-          </p>
-          <p className="text-lg text-black dark:text-gray-400 mt-2 ">
-            Que tal criar o seu primeiro caderno agora?
-          </p>
-          <div className="mt-6">
-            <DialogNewNoteboock
-              trigger={
-                <DrawnButton
-                  variant="primary"
-                  className="flex items-center gap-2"
-                >
-                  Criar Caderno <Plus size={18} />
-                </DrawnButton>
-              }
-            />
-          </div>
-        </div>
-      )}
-      <ToastContainer
-        position="bottom-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </>
-  );
+  return <HomeContent notebooks={notebooks} />;
 }
---
+++ src/app/_components/noteContent.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import React, { useState } from "react";
+import NoteCard from "@/app/_components/cardNote";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Notebook } from "lucide-react";
+import { ToastContainer } from "react-toastify";
+import DrawnButton from "@/app/_components/drawnButton";
+import { emotionTranslations } from "@/app/_constants/emotion";
+import Image from "next/image";
+import Link from "next/link";
+import { NoteModel } from "@prisma/client";
+
+export default function NoteContent({ data }: { data: NoteModel[] }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Filtrando as notas com base no termo de busca
+  const filteredNotes = data.filter((note) =>
+    note.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Contando os sentimentos para exibir no gráfico
+  const emotionCounts = filteredNotes.reduce(
+    (acc: Record<string, number>, note) => {
+      acc[note.Emotion] = (acc[note.Emotion] || 0) + 1;
+      return acc;
+    },
+    {}
+  );
+
+  return (
+    <div className="space-y-6">
+      {/* Cabeçalho */}
+      <div className="flex flex-col items-center mb-8">
+        <h1 className="text-4xl md:text-6xl font-bold text-center font-edu">
+          Todas Notas
+        </h1>
+      </div>
+
+      {/* Campo de busca */}
+      <div className="mt-4 mb-6">
+        <input
+          type="text"
+          placeholder="Buscar notas..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 
+             bg-white text-black placeholder-gray-400 border-gray-300
+             dark:bg-gray-800 dark:text-gray-200 dark:placeholder-gray-500 dark:border-gray-600"
+        />
+      </div>
+
+      {/* Seção de Sentimentos */}
+      {filteredNotes.length > 0 && (
+        <Card className="bg-gray-50 dark:bg-gray-900 shadow-lg rounded-lg">
+          <CardHeader>
+            <CardTitle className="text-center text-2xl font-semibold text-gray-800 dark:text-gray-100">
+              Distribuição de Sentimentos
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-wrap gap-4 justify-center">
+            {Object.entries(emotionCounts).map(([emotion, count]) => (
+              <div
+                key={emotion}
+                className="flex flex-col items-center bg-gray-100 dark:bg-gray-800 p-4 rounded-lg shadow-md space-y-2 w-36"
+              >
+                <span className="text-3xl">
+                  {
+                    emotionTranslations[
+                      emotion as keyof typeof emotionTranslations
+                    ].split(" ")[0]
+                  }
+                </span>
+                <span
+                  className={`capitalize text-lg font-medium text-center ${
+                    count > 0
+                      ? "text-gray-800 dark:text-gray-100"
+                      : "text-gray-400"
+                  }`}
+                >
+                  {emotionTranslations[
+                    emotion as keyof typeof emotionTranslations
+                  ]
+                    .split(" ")
+                    .slice(1)
+                    .join(" ")}
+                </span>
+                <span
+                  className={`inline-flex items-center justify-center px-3 py-1 text-sm font-medium rounded-full ${
+                    count > 0
+                      ? "bg-blue-500 text-white"
+                      : "bg-gray-400 text-gray-200"
+                  }`}
+                >
+                  {Math.round((count / filteredNotes.length) * 100)}%
+                </span>
+                <span className="text-sm text-gray-600 dark:text-gray-400">
+                  {count} nota{count !== 1 ? "s" : ""}
+                </span>
+              </div>
+            ))}
+          </CardContent>
+        </Card>
+      )}
+
+      {/* Seção de Notas */}
+      {filteredNotes.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-0">
+          {filteredNotes.map((note) => (
+            <NoteCard {...note} key={note.id} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center h-[70vh] text-center">
+          <Image
+            src="/empty.svg"
+            alt="No notebooks"
+            className="w-48 h-48 mb-4"
+            width={96}
+            height={96}
+          />
+          <p className="text-2xl md:text-3xl lg:text-4xl font-medium text-black dark:text-gray-400">
+            Nenhuma nota encontrada.
+          </p>
+          <p className="text-lg text-black dark:text-gray-400 mt-2">
+            Comece criando um caderno e crie uma nota para começar!
+          </p>
+          <div className="mt-6">
+            <Link href="/Main/">
+              <DrawnButton
+                variant="primary"
+                className="flex items-center gap-2"
+              >
+                Ir para os Cadernos <Notebook size={18} />
+              </DrawnButton>
+            </Link>
+          </div>
+        </div>
+      )}
+
+      {/* Toast para Notificações */}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
+}
