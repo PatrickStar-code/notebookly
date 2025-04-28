059e0193f377211743da79006979726e21d4410d
Patrick da Silva Almeida <patrick.almeida06@gmail.com>
2025-04-28 14:20:21 -0300

    fix:deploy Error

--- src/app/Main/note/page.tsx
+++ src/app/Main/note/page.tsx
@@ -2,8 +2,11 @@ import getAllNotes from "@/app/_actions/getAllNotes";
 import { NoteModel } from "@prisma/client";
 
 import NoteContent from "@/app/_components/noteContent";
+import { auth } from "@/auth";
 
 export default async function AllNotesPage() {
-  const data: NoteModel[] = await getAllNotes();
+  const session = await auth();
+
+  const data: NoteModel[] = await getAllNotes(session?.user?.id as string);
   return <NoteContent data={data} />;
 }
